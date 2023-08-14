5b839e14c7b1d1636cfd6f4943cfaba804fc3c55
Nikita N <107304449+Qyeler@users.noreply.github.com>
2023-08-14 18:45:16 +0300

    bubble sort moment

--- SelectionSort/selection_sort.hpp
+++ SelectionSort/selection_sort.hpp
@@ -5,5 +5,19 @@
 // https://leetcode.com/problems/sort-an-array/
 class Solution {
 public:
-  std::vector<int> sortArray(std::vector<int> &nums) {}
+  std::vector<int> sortArray(std::vector<int> &nums) {
+      std::vector<int> result=nums;
+     for(int i=0;i<result.size();i++){
+            int index=i;
+            int minn=result[i];
+            for(int j=i;j<result.size();j++){
+                if(result[j]<minn){
+                    minn=result[j];
+                    index=j;
+                }
+            }
+            std::swap(result[index],result[i]);
+        }
+        return(result);
+  }
 };
